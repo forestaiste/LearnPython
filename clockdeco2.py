90da4bd473aa5e225283ec8ea4e5677f736ec431
forestaiste <forestaiste@hotmail.com>
2022-01-22 12:43:38 +0800

    clockdeco.py

--- clockdeco2.py
+++ clockdeco2.py
@@ -18,7 +18,7 @@ def clock(func):
             arg_lst.append(', '.join(pairs))
 
         arg_str = ', '.join(arg_lst)
-        print('[%0.8fs]%s(%s)->%r ' % (elapsed, name, arg_str, result))
+        print('[%0.8fs]%s(%s)->%r' % (elapsed, name, arg_str, result))
         return result
 
     return clocked
--- clockdeco2_demo.py
+++ clockdeco2_demo.py
@@ -1,6 +1,5 @@
 import time
-import functools
-from clockdeco import clock
+from clockdeco2 import clock
 
 
 @clock
@@ -13,18 +12,8 @@ def factorial(n):
     return 1 if n < 2 else n * factorial(n - 1)
 
 
-@clock
-@functools.lru_cache()
-def fibonacci(n):
-    if n < 2:
-        return n
-    return fibonacci(n - 2) + fibonacci(n - 1)
-
-
 if __name__ == '__main__':
     print('*' * 40, 'Calling snooze(.123)')
     snooze(.123)
     print('*' * 40, 'Calling factorial(6)')
     print('6!=', factorial(6))
-    print('*' * 40, 'Calling fibonacci(6)')
-    print(fibonacci(6))
